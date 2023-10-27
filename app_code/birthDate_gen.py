62b16a93b3400b2f41ffbfb2dc36b810c5ddda0b
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2023-10-27 17:22:05 +0200

    adjust the age limit

--- app_code/birthDate_gen.py
+++ app_code/birthDate_gen.py
@@ -5,7 +5,7 @@ def generate_random_birthdate():
 
     day = randint(1, 28)
     month = randint(1, 12)
-    year = randint(1935, 2003)
+    year = randint(1930, 2003)
     birht_date = f'{day:02d}/{month:02d}/{year % 100:02d}'
     return birht_date
 
