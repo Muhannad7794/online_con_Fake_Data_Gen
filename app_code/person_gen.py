b87bd70a1883b6a0e2e4d6118aad1c53b385ea49
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2023-10-31 15:46:07 +0100

    adding the json file with an env var this time to be seen by gh actions

--- app_code/person_gen.py
+++ app_code/person_gen.py
@@ -1,23 +1,28 @@
 import json
 from random import choice
-
-# intelize the json file and get the data
-
+import os
 
 def generate_random_person():
-    try:
-        with open('C:/Muhannad_data/Studying/Projects/Fake_Data_Generator/sorce_data/person-names.json', encoding='utf-8-sig') as persons_data:
-            data = json.load(persons_data)
-            persons = data['persons']
-            random_person = choice(persons)
-            surname = random_person['surname']
-            name = random_person['name']
-            gender = random_person['gender']
+    json_path = os.getenv('JSON_PATH_GITHUB')
 
-    except FileNotFoundError:
-        print("File not found, please check the path")
+    if json_path:
+        file_path = os.path.join(json_path, 'person-names.json')
+    
     else:
-        return name, surname, gender
+        
+        try:
+            with open('C:/Muhannad_data/Studying/Projects/Fake_Data_Generator/sorce_data/person-names.json', encoding='utf-8-sig') as persons_data:
+                data = json.load(persons_data)
+                persons = data['persons']
+                random_person = choice(persons)
+                surname = random_person['surname']
+                name = random_person['name']
+                gender = random_person['gender']
+
+        except FileNotFoundError:
+            print("File not found, please check the path")
+        else:
+            return name, surname, gender
 
 
 person_1 = generate_random_person()
