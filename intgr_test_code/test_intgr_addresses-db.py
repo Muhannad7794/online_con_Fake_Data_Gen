4d477bb79d3e913ba115493f2c8a89862a1e65c3
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2023-11-01 15:39:59 +0100

    adjuted the coon_str in the intgr test files so they can run on gh actionns configs

--- intgr_test_code/test_intgr_addresses-db.py
+++ intgr_test_code/test_intgr_addresses-db.py
@@ -1,31 +1,35 @@
 import pyodbc
+import os
 from app_code.addresses_main_gen import generate_random_address
 
 def test_integration_generate_random_address():
-    try:
-        # Arrange: Set up connection parameters
-        server = 'your_server_name'
-        database = 'your_database_name'
-        username = 'your_username'
-        password = 'your_password'
-        conn_str = f'Driver={{ODBC Driver 18 for SQL Server}};' \
-                   f'Server=tcp:kea-projects-sql-server.database.windows.net;' \
-                   f'Database=addresses_main;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;' \
-                   f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    if os.environ.get('GITHUB_ACTIONS') == 'true':
+        conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};Server=tcp:kea-projects-sql-server.database.windows.net,1433;Database=addresses_main;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    else:
+        try:
+            # Arrange: Set up connection parameters
+            server = 'your_server_name'
+            database = 'your_database_name'
+            username = 'your_username'
+            password = 'your_password'
+            conn_str = f'Driver={{ODBC Driver 18 for SQL Server}};' \
+                    f'Server=tcp:kea-projects-sql-server.database.windows.net;' \
+                    f'Database=addresses_main;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;' \
+                    f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
-        # Act: Test connection and data retrieval
-        with pyodbc.connect(conn_str) as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM postal_code')  
+            # Act: Test connection and data retrieval
+            with pyodbc.connect(conn_str) as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT * FROM postal_code')  
 
-            # Assert: Verify data retrieval
-            rows = cursor.fetchall()
-            assert len(rows) > 0  # Check if data was retrieved
+                # Assert: Verify data retrieval
+                rows = cursor.fetchall()
+                assert len(rows) > 0  # Check if data was retrieved
 
-    except pyodbc.Error as e:
-        # If an exception occurs, handle it here
-        assert False, f'Error: {e}'
+        except pyodbc.Error as e:
+            # If an exception occurs, handle it here
+            assert False, f'Error: {e}'
 
-    except Exception as e:
-        # Handle any other exceptions here
-        assert False, f'Error: {e}'
+        except Exception as e:
+            # Handle any other exceptions here
+            assert False, f'Error: {e}'
--- intgr_test_code/test_intgr_people-db.py
+++ intgr_test_code/test_intgr_people-db.py
@@ -1,30 +1,34 @@
 import pyodbc
+import os
 
 def test_integration_generate_random_person():
-    try:
-        # Arrange: Set up connection parameters
-        server = 'your_server_name'
-        database = 'your_database_name'
-        username = 'your_username'
-        password = 'your_password'
-        conn_str = f'Driver={{ODBC Driver 18 for SQL Server}};' \
-                   f'Server=tcp:kea-projects-sql-server.database.windows.net;' \
-                   f'Database=people;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;' \
-                   f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    if os.environ.get('GITHUB_ACTIONS') == 'true':
+        conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};Server=tcp:kea-projects-sql-server.database.windows.net,1433;Database=people;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    else:
+        try:
+            # Arrange: Set up connection parameters
+            server = 'your_server_name'
+            database = 'your_database_name'
+            username = 'your_username'
+            password = 'your_password'
+            conn_str = f'Driver={{ODBC Driver 18 for SQL Server}};' \
+                    f'Server=tcp:kea-projects-sql-server.database.windows.net;' \
+                    f'Database=people;Uid=mual_sql_admin;Pwd=m.u.a.l_KEA_server_access;' \
+                    f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
-        # Act: Test connection and data retrieval
-        with pyodbc.connect(conn_str) as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM persons')  
+            # Act: Test connection and data retrieval
+            with pyodbc.connect(conn_str) as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT * FROM persons')  
 
-            # Assert: Verify data retrieval
-            rows = cursor.fetchall()
-            assert len(rows) > 0  # Check if data was retrieved
+                # Assert: Verify data retrieval
+                rows = cursor.fetchall()
+                assert len(rows) > 0  # Check if data was retrieved
 
-    except pyodbc.Error as e:
-        # If an exception occurs, handle it here
-        assert False, f'Error: {e}'
+        except pyodbc.Error as e:
+            # If an exception occurs, handle it here
+            assert False, f'Error: {e}'
 
-    except Exception as e:
-        # Handle any other exceptions here
-        assert False, f'Error: {e}'
+        except Exception as e:
+            # Handle any other exceptions here
+            assert False, f'Error: {e}'
